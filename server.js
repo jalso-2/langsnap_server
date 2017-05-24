51dcaa113e0da9205efe5e6969272037603e1db9
Zachary Bergmann <Zachary Bergmann>
2017-05-24 17:32:22 -0500

    (feat) Make db schema creation calls async

--- server.js
+++ server.js
@@ -35,50 +35,30 @@ Card.belongsToMany(User, { through: UserCard });
 Deck.belongsTo(User);
 
 // Perform Sync
-User.sync();
-Deck.sync();
-Card.sync();
-DeckCard.sync();
-UserCard.sync();
+const syncDb = async () => {
+  await User.sync();
+  await Deck.sync();
+  await Card.sync();
+  await UserCard.sync();
+  await DeckCard.sync();
+};
+syncDb();
 
 server.get('/', (req, res) => res.status(200).send('hello'));
 
-server.post('/user', (req, res) => {
+server.post('/v1/user', async (req, res) => {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const username = req.body.username;
   const email = req.body.email;
-  User.findOrCreate({ where: { email }, defaults: { firstName, lastName, username } })
-    .then((suc) => {
-      console.log(suc, 'this is success');
-      res.status(200).send('Successfully added user to database');
-    })
-    .catch(err => console.error(err));
+  try {
+    const user = await User.findOrCreate({ where: { email }, defaults: { firstName, lastName, username } });
+    return res.status(200).send(user[0]);
+  } catch (err) {
+    return console.error(err);
+  }
 });
 
-server.post('/userdeck', (req, res) => {
-  const name = req.body.name;
-  const userId = req.body.userId;
-  const stars = 0;
-  UserDeck.findOrCreate({ where: { name, userId, stars } })
-    .then((suc) => {
-      console.log(suc, 'this is success creating a user deck');
-      res.status(200).send('Successfully added a new deck to database');
-    })
-    .catch(err => console.error(err));
-});
 
-server.post('/card', (req, res) => {
-  const imgUrl = req.body.imgUrl;
-  const deckId = req.body.deckId;
-  const stars = 0;
-  // Get the wordMap from proper API
-  // After this, add the card to the UserDeck with id and add a UserCardInfo with the id
-  // Add to join table??? think yes
-});
-
-// server.get('/v1/decks/deckid/*', (req, res) => {
-
-// });
 
 // ///////////////////////// END ENDPOINTS /////////////////////////////////
