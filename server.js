6e3dc0c64a65d7e775ac765b42165ee53bef17d0
xerra <zbbergma@gmail.com>
2017-05-25 21:04:11 -0500

    (prog) Continue to work on endpoints

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const axios = require('axios');
 const Sequelize = require('sequelize');
+const bluebird = require('bluebird');
 
 const PORT = process.env.PORT || 8080;
 const sequelize = new Sequelize(process.env.DATABASE_URL);
@@ -57,7 +58,7 @@ syncDb();
 // ///////////////////////// START ENDPOINTS ///////////////////////////////
 server.get('/', (req, res) => res.status(200).send('hello'));
 
-server.post('/v1/users', async (req, res) => { // goodish, could filter a bit more!
+server.post('/v1/users', async (req, res) => { // goodish, should this be createOrUpdate???
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const username = req.body.username;
@@ -160,30 +161,28 @@ server.get('/v1/decks/userid/*', async (req, res) => {  // good!
   }
 });
 
-server.get('/v1/cards/all', async (req, res) => {  //works, filter a bit
+server.get('/v1/cards/all', async (req, res) => {  // good!
   try {
-    const cards = await Card.findAll();
+    const cards = await Card.findAll({ attributes: ['id', 'imgUrl', 'stars'] });
     return res.status(200).send(cards);
   } catch (err) {
     return res.status(400).send(err);
   }
 });
 
-server.get('/v1/cards/deckid/*', async (req, res) => {  // works, condense results
+server.get('/v1/cards/deckid/*', async (req, res) => {  // good!
   const id = +req.params[0];
   try {
-    const deck = await Deck.findAll({ include: [Card], where: { id } });
+    const deck = await Deck.findAll({
+      include: [{
+        model: Card,
+        attributes: ['id', 'imgUrl', 'wordMap', 'stars'],
+        through: { attributes: ['lastVisited', 'timeInterval', 'phrase'] },
+      }],
+      where: { id },
+      attributes: ['id', 'name'],
+    });
     return res.status(200).send(deck);
-  } catch (err) {
-    return res.status(400).send(err);
-  }
-});
-
-server.get('/v1/cards/userid/*', async (req, res) => { // works so far, want to condense and retest
-  const id = req.params[0];
-  try {
-    const cards = await User.findAll({ include: [Deck, Card], where: { id } });
-    return res.status(200).send(cards);
   } catch (err) {
     return res.status(400).send(err);
   }
@@ -201,7 +200,7 @@ server.post('/v1/decks/new', async (req, res) => {  // works, condense
   }
 });
 
-server.post('/v1/cards/addcard', async (req, res) => { // works, condense
+server.post('/v1/cards/addcard', async (req, res) => { // works, condense, default join table
   const user_id = req.body.user_id;
   const imgUrl = req.body.imgUrl;
   const wordMap = req.body.wordMap;
@@ -224,4 +223,40 @@ server.post('/v1/cards/addcard', async (req, res) => { // works, condense
     return res.status(400).send(err);
   }
 });
+
+server.post('/v1/decks/adddecks', async (req, res) => {
+  const user_id = req.body.id;
+  const decks = JSON.parse(req.body.decks);
+  const createdDecksOutput = [];
+  await Promise.all(decks.map(async (deckId) => {
+    try {
+      const originalDeck = await Deck.findOne({ where: { id: deckId } });
+      const name = originalDeck.name;
+      try {
+        const createdDeck = await Deck.create({ user_id, name, stars: 0 });
+        return createdDecksOutput.push(createdDeck);
+      } catch (err) {
+        return res.status(400).send(err);
+      }
+    } catch (error) {
+      return res.status(400).send('error');
+    }
+  }));
+  return res.status(200).send(createdDecksOutput);
+});
+
+// server.post('/v1/decks/addcards', async (req, res) => {
+
+// });
+
+// server.delete('/v1/decks/*', async (req, res) => {
+
+// });
+
+// server.delete('/v1/cards/*', async (req, res) => {
+
+// });
+
+
+
 // ///////////////////////// END ENDPOINTS /////////////////////////////////
