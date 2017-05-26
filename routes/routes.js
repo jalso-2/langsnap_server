f32223ef43da67b5c906b2732d4daafff51c8fc3
xerra <zbbergma@gmail.com>
2017-05-26 13:12:40 -0500

    (maint) Start refactor of code to separate db methods and routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,65 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const axios = require('axios');
-const Sequelize = require('sequelize');
-const bluebird = require('bluebird');
+const server = require('../server').server;
+const dbHelpers = require('../db_helpers/db_helpers');
+const Card = require('../card/card_schema');
+const Deck = require('../deck/deck_schema');
 
-const PORT = process.env.PORT || 8080;
-const sequelize = new Sequelize(process.env.DATABASE_URL);
-module.exports.sequelize = sequelize;
-const server = express();
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-
-
-
-// ///////////////////////// START MIDDLEWARE ///////////////////////////////
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: false }));
-server.use(morgan('dev'));
-// server.use(require('./routes/routes'));
-
-// ///////////////////////// END MIDDLEWARE ////////////////////////////////
-
-
-
-
-
-// ///////////////////////// START DATABASE RELATIONSHIPS ///////////////////
-const User = require('./user/user_schema');
-const Deck = require('./deck/deck_schema');
-const Card = require('./card/card_schema');
-const DeckCard = require('./deck_card/deck_card_schema');
-const UserCard = require('./user_card/user_card_schema');
-
-// Relationships
-User.hasMany(Deck, { onDelete: 'CASCADE' });
-Card.belongsToMany(Deck, { onDelete: 'CASCADE', through: DeckCard });
-Card.belongsToMany(User, { onDelete: 'CASCADE', through: UserCard });
-Deck.belongsTo(User, { onDelete: 'CASCADE' });
-Deck.belongsToMany(Card, { onDelete: 'CASCADE', through: DeckCard });
-User.belongsToMany(Card, { onDelete: 'CASCADE', through: UserCard });
-
-// Perform sync of models with existing database models
-const syncDb = async () => {
-  await User.sync();
-  await Deck.sync();
-  await Card.sync();
-  await UserCard.sync();
-  await DeckCard.sync();
-};
-syncDb();
-
-const dbHelpers = require('./db_helpers/db_helpers');
-// /////////////////////// END DATABASE RELATIONSHIPS ///////////////////////
-
-
-
-
-
-// ///////////////////////// START ENDPOINTS ///////////////////////////////
 server.get('/', (req, res) => res.status(200).send('hello'));
 
 server.post('/v1/users', (req, res) => { // goodish, should this be createOrUpdate???
@@ -166,6 +109,3 @@ server.delete('/v1/cards/*', async (req, res) => {  // good!
   const id = req.params[0];
   return dbHelpers.deleteCardById(id, res);
 });
-
-
-// ///////////////////////// END ENDPOINTS /////////////////////////////////
