a2560e2fdbd87214b6e87abd4835f0f31c08c034
xerra <zbbergma@gmail.com>
2017-05-30 11:19:09 -0500

    (bugfix) Get join table extra properties updating

--- db_schema/db_helpers_fxns.js
+++ db_schema/db_helpers_fxns.js
@@ -1,5 +1,4 @@
 const bluebird = require('bluebird');
-const axios = require('axios');
 const User = require('./user/user_schema');
 const Card = require('./card/card_schema');
 const Deck = require('./deck/deck_schema');
@@ -7,61 +6,6 @@ const DeckCard = require('./deck_card/deck_card_schema');
 const UserCard = require('./user_card/user_card_schema');
 
 module.exports = {
-  sendUrlToGoogleVisionForName: (url, res) => {
-    axios({
-      method: 'post',
-      url: `https://vision.googleapis.com/v1/images:annotate?key=${process.env.GOOGLE_VISION_KEY}`,
-      data: {
-        requests: [
-          {
-            image: {
-              source: {
-                imageUri:
-                  url,
-              },
-            },
-            features: [
-              {
-                type: 'LABEL_DETECTION',
-                maxResults: 5,
-              },
-            ],
-          },
-        ],
-      },
-    })
-    .then(resp =>
-      res.status(200).send(resp.data.responses[0].labelAnnotations[0].description))
-    .catch(err =>
-      res.status(400).send(err));
-  },
-  getGoogleTranslateOfSentence: (q, source, target, res) => {
-    axios.post(`https://translation.googleapis.com/language/translate/v2?key=${process.env.GOOGLE_VISION}`,
-      {
-        q,
-        source,
-        target,
-        format: 'text',
-      })
-        .then(transData => res.status(200).send(transData.data))
-        .catch(err => res.status(400).send(err));
-  },
-  getSamplePhraseFromWordWordnik: (queryWord, res) => {
-    axios.get(`http://api.wordnik.com:80/v4/word.json/${queryWord}/examples?includeDuplicates=false&useCanonical=false&skip=0&limit=5&api_key=${process.env.WORDNIK_KEY}`)
-      .then(response => res.status(200).send(response.data))
-      .catch(err => res.status(400).send(err));
-  },
-  getSamplePhraseEnglishFromWordOxford: (queryWord, res) => {
-    axios.get(`https://od-api.oxforddictionaries.com:443/api/v1/entries/en/${queryWord}/sentences`, {
-      headers: {
-        Accept: 'application/json',
-        app_id: process.env.OXFORD_APP_ID,
-        app_key: process.env.OXFORD_APP_KEY,
-      },
-    })
-      .then(response => res.status(200).send(response.data))
-      .catch(err => res.status(400).send(err));
-  },
   getAllCardsFromDeckByDeckId: async (id, res) => {
     try {
       const deck = await Deck.findAll({
@@ -221,8 +165,19 @@ module.exports = {
       try {
         await card.addUser(user_id);
         try {
-          await card.addDeck(deck_id);
-          return res.status(200).send(card);
+          const deck = await Deck.findOne({ where: { id: deck_id } });
+          try {
+            await card.addDeck(deck, {  // changed, do it work?
+              timeInterval: 3000,
+              phrase: '',
+              lastVisited: (new Date()).toISOString(),
+              card_id: card.id,
+              deck_id: deck.id,
+            });
+            return res.status(200).send(card);
+          } catch (er) {
+            return res.status(400).send(er);
+          }
         } catch (error) {
           return res.status(500).send(error);
         }
