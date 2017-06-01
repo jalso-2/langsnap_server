f26a751969b92f7be6ff6e80ca08d781049bb3d5
xerra <zbbergma@gmail.com>
2017-06-01 14:10:57 -0500

    (prog) Start readme file for MVP presentation

--- db_schema/db_seeder/db_seeder.js
+++ db_schema/db_seeder/db_seeder.js
@@ -21,10 +21,10 @@ const dbSeeders = require('./db_seeder_helpers');
     dbSeeders.userCreateNewDeck('EmanDeck1', 5, 89);
     dbSeeders.userCreateNewDeck('EmanDeck2', 5, 312);
 
-    dbSeeders.userAddCreatedCardToDeck(1, 'https://art-s.nflximg.net/fe3ed/9568ac3ce5eed5e4fc7d91e8ac2a5114d12fe3ed.jpg', '{"en":"not!"}', 1);
+    dbSeeders.userAddCreatedCardToDeck(1, 'http://res.cloudinary.com/dzwquspqa/image/upload/v1494294356/ifi0doxcpmqh8ervk7ga.jpg', '{"en":"not!"}', 1);
     dbSeeders.userAddCreatedCardToDeck(1, 'https://art-s.nflximg.net/fe3ed/9568ac3ce5eed5e4fc7d91e8ac2a5114d12fe3ed.jpg', '{"es":"si!"}', 1);
     dbSeeders.userAddCreatedCardToDeck(1, 'https://art-s.nflximg.net/fe3ed/9568ac3ce5eed5e4fc7d91e8ac2a5114d12fe3ed.jpg', '{"ru":"ple!"}', 2);
-    dbSeeders.userAddCreatedCardToDeck(2, 'https://art-s.nflximg.net/fe3ed/9568ac3ce5eed5e4fc7d91e8ac2a5114d12fe3ed.jpg', '{"en":"gdsaf!"}', 3);
+    dbSeeders.userAddCreatedCardToDeck(2, 'https://vetstreet.brightspotcdn.com/dims4/default/a1a90c7/2147483647/thumbnail/180x180/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F0d%2Ff2e4c0b3a611e092fe0050568d634f%2Ffile%2Fhub-cats-senior.jpg', '{"en":"gdsaf!"}', 3);
     dbSeeders.userAddCreatedCardToDeck(2, 'https://art-s.nflximg.net/fe3ed/9568ac3ce5eed5e4fc7d91e8ac2a5114d12fe3ed.jpg', '{"es":"n!"}', 3);
     dbSeeders.userAddCreatedCardToDeck(2, 'https://art-s.nflximg.net/fe3ed/9568ac3ce5eed5e4fc7d91e8ac2a5114d12fe3ed.jpg', '{"fr":"non!"}', 4);
     dbSeeders.userAddCreatedCardToDeck(2, 'https://art-s.nflximg.net/fe3ed/9568ac3ce5eed5e4fc7d91e8ac2a5114d12fe3ed.jpg', '{"en":"yay!"}', 4);
@@ -39,7 +39,7 @@ const dbSeeders = require('./db_seeder_helpers');
     dbSeeders.userAddCreatedCardToDeck(5, 'https://art-s.nflximg.net/fe3ed/9568ac3ce5eed5e4fc7d91e8ac2a5114d12fe3ed.jpg', '{"ll":"nolllllt!"}', 8);
     dbSeeders.userAddCreatedCardToDeck(5, 'https://art-s.nflximg.net/fe3ed/9568ac3ce5eed5e4fc7d91e8ac2a5114d12fe3ed.jpg', '{"ff":"fff!"}', 8);
     dbSeeders.userAddCreatedCardToDeck(5, 'https://art-s.nflximg.net/fe3ed/9568ac3ce5eed5e4fc7d91e8ac2a5114d12fe3ed.jpg', '{"zz":"zzz!"}', 9);
-    dbSeeders.userAddCreatedCardToDeck(5, 'https://art-s.nflximg.net/fe3ed/9568ac3ce5eed5e4fc7d91e8ac2a5114d12fe3ed.jpg', '{"jj":"jjj!"}', 9);
+    dbSeeders.userAddCreatedCardToDeck(5, 'https://vetstreet.brightspotcdn.com/dims4/default/a1a90c7/2147483647/thumbnail/180x180/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F0d%2Ff2e4c0b3a611e092fe0050568d634f%2Ffile%2Fhub-cats-senior.jpg', '{"jj":"jjj!"}', 9);
   } catch (err) {
     console.log('Error while seeding DB, make sure the check PSQL');
   }
