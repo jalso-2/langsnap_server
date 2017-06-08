f19cb1af753db2a4d75fe9543f259db4ceef855d
xerra <zbbergma@gmail.com>
2017-06-08 11:21:44 -0500

    (bugfix) Change error handling in user auth

--- routes.js
+++ routes.js
@@ -92,7 +92,7 @@ router.get('/v1/users/auth/*/*', async (req, res) => {
   const username = decodeURI(req.params[1]);
   if (socialLoginSource === 'facebook') {
     const result = await dbHelpers.findUserIfExistsBySocialId(socialLoginSource, username);
-    return result instanceof Error ? res.status(400).send(result) : res.status(200).send(result);
+    return typeof result === 'string' ? res.status(400).send('Error checking user authentication') : res.status(200).send(result);
   }
   return res.sendStatus(400);
 });
